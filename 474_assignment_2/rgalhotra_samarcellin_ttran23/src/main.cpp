a8569d85af5c72b2a3e7f5057240b84908cabd4c
Rohin Galhotra <jiminycracken@gmail.com>
2017-10-22 19:33:14 -0700

    WAKE ME UP INSIDE

--- 474_assignment_2/rgalhotra_samarcellin_ttran23/src/main.cpp
+++ 474_assignment_2/rgalhotra_samarcellin_ttran23/src/main.cpp
@@ -8,7 +8,7 @@
 #include "node.h"
 #include "verilogOutput.h"
 
-int main (int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 	std::istringstream inSS;
 	std::ifstream inFS;
 	std::string line;
@@ -23,64 +23,73 @@ int main (int argc, char* argv[]){
 	int i, j, size, num = 0;
 	char name;
 	bool SIGN;
+	bool clkrst;
 
-	if(argc != 3){  //check for correct input
+	if (argc != 3) {  //check for correct input
 		std::cout << "\nUSAGE: dpgen netlistFile verilogFile\n\n";
 		return -1;
 	}
 
 	inFS.open(argv[1]);//open input file
-	if(!inFS.is_open()){ //check opened correctly
+	if (!inFS.is_open()) { //check opened correctly
 		std::cout << "Could not open input file." << std::endl;
 		return -1;
 	}
 
-	while(!inFS.eof()){ //process file
+	while (!inFS.eof()) { //process file
 		getline(inFS, line);
 		inSS.clear();
 		inSS.str(line);
-		if(!line.empty()){ //if line isn't empty (we ignore empty lines)
+		if (!line.empty()) { //if line isn't empty (we ignore empty lines)
 			std::istringstream iss(line);
 			std::vector<std::string> results((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());  //break it into words
-			if((results[0] == "input")||(results[0] == "output")||(results[0] == "wire")||(results[0] == "register")){ //get data types and variable names of inputs and outputs
-				//parse data for node
+			if ((results[0] == "input") || (results[0] == "output") || (results[0] == "wire") || (results[0] == "register")) { //get data types and variable names of inputs and outputs
+																															   //parse data for node
 				temp = results[1];
-				if(temp[0] == 'U'){
+				if (temp[0] == 'U') {
 					SIGN = 0; //unsigned 
 				}
-				else{
+				else {
 					SIGN = 1; //signed
 				}
 				j = 0;
-				for(i = 0; i < temp.size(); ++i){ //get dataSize by removing letters from Int32, etc.
-					if (isdigit(temp[i])){
-						 temp[j] = temp[i];
-						 j++;
-					 }
+				for (i = 0; i < temp.size(); ++i) { //get dataSize by removing letters from Int32, etc.
+					if (isdigit(temp[i])) {
+						temp[j] = temp[i];
+						j++;
+					}
 				}
-				while(j < temp.size()){
+				while (j < temp.size()) {
 					temp[j] = NULL;
 					j++;
 				}
-				std::istringstream (temp) >> size;
-				for(i = 2; i < results.size(); i++){ //create a node for each variable with these stats
+				std::istringstream(temp) >> size;
+				for (i = 2; i < results.size(); i++) { //create a node for each variable with these stats
 					temp = results[i]; //get rid of commas
-					if(temp[1] == ','){			// Setting to NULL messes up the output to file, but since it looked like you were just setting temp[1]
+					if (temp[1] == ',') {			// Setting to NULL messes up the output to file, but since it looked like you were just setting temp[1]
 						results[i] = temp[0];	// to NULL, it would mean the message would just end before that, so temp[0] is effectively the same.
 					}
 					else {
 						results[i] = temp;
 					}
-					list.push_back(node(results[0],results[i],SIGN,size));
+					list.push_back(node(results[0], results[i], SIGN, size));
 				}
 			}
 			else if (results[1] == "=") {
-				if (results[3] == "") { //need to find a better way to detect registers
+				std::cout << results[3] << std::endl; //for debugging
+				if (results[3].empty()) {
+					std::cout << "this actually happens" << std::endl;
 					result = results[0];
 					oper1 = results[2];
 					type = "reg";
-					list2.push_back(generateModule(result, oper1, "", "reg", num, list));
+					std::cout << type << std::endl;
+					list2.push_back(generateModule(result, oper1, "help", type, num, list));
 					num++;
+					if (!clkrst) {
+						list.push_back(node("input", "Clk", true, 1));
+						list.push_back(node("input", "Rst", true, 1));
+						clkrst = true;
+					}
 				}
 				else if (results[3] == "?") { //how do we handle the module names?
 					result = results[0];
@@ -96,16 +105,18 @@ int main (int argc, char* argv[]){
 					oper1 = results[2];
 					oper2 = results[4];
 					type = results[3];
+					std::cout << results[3] << "this" << std::endl;
+					std::cout << type << "sucks" << std::endl;
 					list2.push_back(generateModule(result, oper1, oper2, type, num, list));
 					num++;
 				}
 			}
 		}
 	}
-	
+
 
 	//test print of list
-	for(i = 0; i < list.size(); i++){
+	for (i = 0; i < list.size(); i++) {
 		std::cout << list[i].getName() << '\t' << list[i].getType() << '\t' << list[i].getSIGN() << '\t' << list[i].getDataSize() << std::endl;
 	}
 	std::cout << "\n\n\n";
--- 474_assignment_2/rgalhotra_samarcellin_ttran23/src/verilogOutput.cpp
+++ 474_assignment_2/rgalhotra_samarcellin_ttran23/src/verilogOutput.cpp
@@ -10,7 +10,7 @@ void generateIO(std::vector<node> list, char* outFileStr) {
 
 	// Looping through
 	for (int i = 0; i < list.size(); i++) {
-	//list[i].getName() << list[i].getType() << list[i].getSIGN() << list[i].getDataSize() 
+		//list[i].getName() << list[i].getType() << list[i].getSIGN() << list[i].getDataSize() 
 		if (list.at(i).getType().compare("wire") == 0) {
 			ss = std::stringstream();	// Clear string stream
 			ss << "wire [" << list.at(i).getDataSize() - 1 << ":0] " << list.at(i).getName() << ';' << std::endl;
@@ -68,9 +68,13 @@ std::string generateModule(std::string result, std::string oper1, std::string op
 			if (ioList.at(i).getDataSize() > datawidth) datawidth = ioList.at(i).getDataSize();
 		}
 	}
-	if (!real1 || !real2 || !real3) return "error"; //error case 1-3
+	std::cout << sign1 << sign2 << std::endl;
+	std::cout << type << "what" << std::endl;
+	std::cout << !real1 << !(real2 || type == "reg") << !real3 << std::endl;
+	if (!real1 || !(real2 || type == "reg") || !real3) return "error"; //error case 1-3
+	std::cout << type << std::endl;
 	if (type == "+") {
-		if(oper2 == "1") out = "INC #(.DATAWIDTH(" + std::to_string(datawidth) + ")) incrementor" + std::to_string(num) + "(" + oper1 + "," + result + ");";
+		if (oper2 == "1") out = "INC #(.DATAWIDTH(" + std::to_string(datawidth) + ")) incrementor" + std::to_string(num) + "(" + oper1 + "," + result + ");";
 		else out = "ADD #(.DATAWIDTH(" + std::to_string(datawidth) + ")) adder" + std::to_string(num) + "(" + oper1 + "," + oper2 + "," + result + ");";
 	}
 	else if (type == "-") {
@@ -78,7 +82,8 @@ std::string generateModule(std::string result, std::string oper1, std::string op
 		else out = "SUB #(.DATAWIDTH(" + std::to_string(datawidth) + ")) subber" + std::to_string(num) + "(" + oper1 + "," + oper2 + "," + result + ");";
 	}
 	else if (type == "reg") {
-		out = "REG #(.DATAWIDTH(" + std::to_string(datawidth) + ")) register" + std::to_string(num) + "(Clk, Rst" + "," + oper1 + "," + result + ");";
+		std::cout << "we got here" << std::endl;
+		out = "REG #(.DATAWIDTH(" + std::to_string(datawidth) + ")) register" + std::to_string(num) + "(Clk,Rst" + "," + oper1 + "," + result + ");";
 	}
 	else if (type == "*") {
 		out = "MUL #(.DATAWIDTH(" + std::to_string(datawidth) + ")) multiplier" + std::to_string(num) + "(" + oper1 + "," + oper2 + "," + result + ");";
@@ -143,12 +148,12 @@ std::string generateMux(std::string result, std::string oper1, std::string oper2
 void generateVerilogFile(std::vector<node> ioList, std::vector<std::string> moduleList, char* inFileStr, char* outFileStr) {
 	// https://stackoverflow.com/questions/8520560/get-a-file-name-from-a-path
 	std::string moduleName = inFileStr;
-	moduleName = moduleName.substr(moduleName.find_last_of("/\\") + 1);	
+	moduleName = moduleName.substr(moduleName.find_last_of("/\\") + 1);
 	moduleName = moduleName.substr(0, moduleName.find_last_of("."));
 
 	std::ofstream outFS;
 	outFS.open(outFileStr);//open output file
-	if (!outFS.is_open()){ //check opened correctly
+	if (!outFS.is_open()) { //check opened correctly
 		std::cout << "Could not open output file." << std::endl;
 		exit(EXIT_FAILURE);
 	}
@@ -160,12 +165,12 @@ void generateVerilogFile(std::vector<node> ioList, std::vector<std::string> modu
 			outFS << ioList.at(i).getName() << ", ";
 		}
 	}
-	outFS << ioList.at(ioList.size()-1).getName() << " );\n";
+	outFS << ioList.at(ioList.size() - 1).getName() << " );\n";
 	outFS.close();
 	generateIO(ioList, outFileStr);
 
 	outFS.open(outFileStr, std::ios::app);
-	for (int i = 0; i < moduleList.size() - 1; i++) {
+	for (int i = 0; i < moduleList.size(); i++) {
 		outFS << moduleList.at(i) << std::endl;
 	}
 	outFS << "endmodule" << std::endl;
