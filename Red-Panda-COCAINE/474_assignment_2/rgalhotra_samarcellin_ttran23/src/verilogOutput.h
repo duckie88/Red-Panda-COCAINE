9f01c48ae4bae9fe6c74402b28ed1010eafd0418
duckie88 <steph.marcellin@gmail.com>
2017-10-26 21:57:54 -0700

    bug squashing

---
+++ Red-Panda-COCAINE/474_assignment_2/rgalhotra_samarcellin_ttran23/src/verilogOutput.h
@@ -0,0 +1,16 @@
+#ifndef VERILOGOUTPUT_H
+#define VERILOGOUTPUT_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "nodes.h"
+
+// Call it as: generateVerilogFile(argv[2]);
+void generateVerilogFile(std::vector<node> ioList, std::vector<std::string> moduleList, char* inFileStr, char* outFileStr);
+void generateIO(std::vector<node> list, char* outFileStr);
+std::string generateModule(std::string result, std::string oper1, std::string oper2, std::string type, int num, std::vector<node>ioList);
+std::string generateMux(std::string result, std::string oper1, std::string oper2, std::string oper3, int num, std::vector<node>ioList);
+#endif // VERILOGOUTPUT_H
